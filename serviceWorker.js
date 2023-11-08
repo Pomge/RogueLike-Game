4db6f0ba1860c3f71568279bf26b3a965bd49168
Pomge <aleks.berkovets@yandex.ru>
2023-11-08 17:29:36 +0300

    Trying to catch an exeption

--- serviceWorker.js
+++ serviceWorker.js
@@ -67,6 +67,8 @@ async function addResourcesToCache(resources) {
 }
 
 async function cacheFirst(request) {
+  console.log(request);
+  console.log(request.url);
   const cached = await caches.match(request);
   return cached ?? (await fetch(request));
 }
