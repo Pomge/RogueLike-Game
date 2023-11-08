d9e069c9d5f677ea365c5e6581a381e6e78e885d
Pomge <aleks.berkovets@yandex.ru>
2023-11-08 17:13:15 +0300

    Trying to fix routes #2

--- js/main/index.js
+++ js/main/index.js
@@ -95,9 +95,7 @@ window.addEventListener("resize", (event) => {
 const registerServiceWorker = async () => {
   if ("serviceWorker" in navigator) {
     try {
-      await navigator.serviceWorker.register("/serviceWorker.js", {
-        scope: "/",
-      });
+      await navigator.serviceWorker.register("./serviceWorker.js");
     } catch (error) {
       console.error(`Service Worker registration failed with ${error}`);
     }
