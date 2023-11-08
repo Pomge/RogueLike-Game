4f7690647be72f54d003b121f4a8d18f80e0b496
Pomge <aleks.berkovets@yandex.ru>
2023-11-08 17:48:22 +0300

    Test 4

--- serviceWorker.js
+++ serviceWorker.js
@@ -69,6 +69,7 @@ async function addResourcesToCache(resources) {
 }
 
 async function cacheFirst(request) {
+  console.log(request);
   console.log(request.url);
   const cached = await caches.match(request);
   return cached ?? (await fetch(request));
